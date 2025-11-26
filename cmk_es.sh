a6c7793694a914eb498d77f681be8d3b9f079ccf
seven-daffodils <seven-daffodils@users.noreply.github.com>
2025-11-26 16:56:51 -0500

    Extend adjustments

---
+++ cmk_es.sh
@@ -0,0 +1,5 @@
+setxkbmap -model pc105awide \
+          -layout es \
+          -variant cmk_ed_us \
+          -option "" \
+          -option misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu,misc:cmk_curl_dh,misc:sym_wide-105
